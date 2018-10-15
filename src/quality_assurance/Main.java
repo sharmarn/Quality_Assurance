b733dc5b7b5f31e9f3238418cf602ba9adc22fed
Raman Sharma <raman.sharma0096@gmail.com>
2018-10-15 16:12:44 +0200

    Add new class solely for main method

---
+++ src/quality_assurance/Main.java
@@ -0,0 +1,190 @@
+package quality_assurance;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public enum Option {
+		TOTAL_ALIVE, TOTAL_AREA, BOTH
+	}
+
+	public static void main(String[] args) {
+
+		// ################## PROPERTY SETTINGS - BEGIN ##################
+
+		String[] fileNames = { "result_default.ce", "result_radius.ce", "result_in_range.ce", "result_osm_id.ce",
+				"result_random.ce" };
+
+		// Set the total number of centers.
+		final int sumOfCenters = 184;
+
+		// Choose the property, you want to calculate.
+		Option option = Option.TOTAL_AREA;
+
+		// Set the file, you want to get the time intervals from.
+		File inputFileForTimeLimitIntervals = new File("result_default.ce");
+
+		// ################## PROPERTY SETTINGS - END ##################
+
+		// Construct understandable names for the used heuristics.
+		ArrayList<String> heuristics = new ArrayList<String>();
+
+		for (String fileName : fileNames) {
+			fileName = fileName.replace("result_", "").replace(".ce", "").toUpperCase();
+			heuristics.add(fileName);
+		}
+
+		// Construct the header of the used heuristics for the .csv file.
+		String stringOfHeuristics = "";
+
+		for (int i = 0; i < heuristics.size(); i++) {
+			if (i < heuristics.size() - 1) {
+				stringOfHeuristics += heuristics.get(i) + ";";
+			} else {
+				stringOfHeuristics += heuristics.get(i);
+			}
+		}
+
+		List<Double> valuesList = null;
+
+		final int columnIndexForEliminationTime = 4;
+		final int columnIndexForRadius = 5;
+		int totalAlive = 0;
+		double totalArea = 0.0;
+
+		// Set the total number of time limits.
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the maximum number of time limits.");
+		int numberOfTimeLimits = input.nextInt();
+
+		// Initialize an array for keeping the intervals of the time limits.
+		double[] arrayOfTimeLimits = new double[numberOfTimeLimits + 1];
+
+		// Get time limits in intervals.
+		try {
+			List<Double> timeLimitsList = QualityAssurance.loadFirstAndSecondLastEliminationTimesFromColumn(
+					inputFileForTimeLimitIntervals, columnIndexForEliminationTime, " ", sumOfCenters);
+			double intervall = (timeLimitsList.get(1) - timeLimitsList.get(0)) / numberOfTimeLimits;
+			for (int i = 0; i < numberOfTimeLimits + 1; i++) {
+				double timeLimit = intervall * i;
+				arrayOfTimeLimits[i] = timeLimit;
+			}
+		} catch (NumberFormatException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		input.close();
+
+		// From here on, the options are specified.
+		if (option == Option.TOTAL_ALIVE) {
+			File resultFile = new File("result_for_total_alive.csv");
+
+			try {
+				String saveResult = "";
+				BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
+				saveResult = "TIME_LIMIT;" + stringOfHeuristics;
+
+				for (double timeLimit : arrayOfTimeLimits) {
+					saveResult = saveResult + "\n" + timeLimit;
+					for (String fileName : fileNames) {
+						File inputFile = new File(fileName);
+
+						try {
+							totalAlive = QualityAssurance.getTotalAlive(inputFile, columnIndexForEliminationTime, " ",
+									timeLimit, valuesList);
+
+							saveResult = saveResult + ";" + totalAlive;
+
+						} catch (Exception e) {
+							System.err.println("Da ist was schiefgegangen: " + e.getMessage());
+						}
+					}
+				}
+				writer.write(saveResult);
+				writer.close();
+				System.out.println("Calculation of total alive has been finished.");
+			} catch (IOException e) {
+				System.out.println("File I/O error!");
+			}
+		}
+
+		else if (option == Option.TOTAL_AREA) {
+			File resultFile = new File("result_for_total_area.csv");
+
+			try {
+				String saveResult = "";
+				BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
+				saveResult = "TIME_LIMIT;" + stringOfHeuristics;
+
+				for (double timeLimit : arrayOfTimeLimits) {
+					saveResult = saveResult + "\n" + timeLimit;
+					for (String fileName : fileNames) {
+						File inputFile = new File(fileName);
+
+						try {
+							totalArea = QualityAssurance.getTotalArea(inputFile, columnIndexForEliminationTime,
+									columnIndexForRadius, " ", timeLimit, valuesList, valuesList, sumOfCenters);
+
+							saveResult = saveResult + ";" + totalArea;
+
+						} catch (Exception e) {
+							System.err.println("Da ist was schiefgegangen: " + e.getMessage());
+						}
+					}
+				}
+				writer.write(saveResult);
+				writer.close();
+				System.out.println("Calculation of total area has been finished.");
+			} catch (IOException e) {
+				System.out.println("File I/O error!");
+			}
+		}
+
+		else {
+			File resultFile = new File("result_total_alive_and_area.csv");
+			if (fileNames.length == 1) {
+				for (String fileName : fileNames) {
+					File inputFile = new File(fileName);
+
+					try {
+						String saveResult = "";
+						BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
+						saveResult = "TIME_LIMIT;TOTAL_ALIVE;TOTAL_AREA";
+
+						for (double timeLimit : arrayOfTimeLimits) {
+
+							try {
+								totalAlive = QualityAssurance.getTotalAlive(inputFile, columnIndexForEliminationTime,
+										" ", timeLimit, valuesList);
+							} catch (Exception e) {
+								System.err.println("Da ist was schiefgegangen: " + e.getMessage());
+							}
+
+							try {
+								totalArea = QualityAssurance.getTotalArea(inputFile, columnIndexForEliminationTime,
+										columnIndexForRadius, " ", timeLimit, valuesList, valuesList, sumOfCenters);
+							} catch (Exception e) {
+								System.err.println("Da ist was schiefgegangen: " + e.getMessage());
+							}
+							saveResult = saveResult + "\n" + timeLimit + ";" + totalAlive + ";" + totalArea;
+						}
+						writer.write(saveResult);
+						writer.close();
+						System.out.println("Calculation of total alive and total area has been finished.");
+					} catch (IOException e) {
+						System.out.println("File I/O error!");
+					}
+				}
+			} else
+				System.out.println("Error: More than one file !");
+		}
+	}
+}
--- src/quality_assurance/QualityAssurance.java
+++ src/quality_assurance/QualityAssurance.java
@@ -1,23 +1,16 @@
 package quality_assurance;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class QualityAssurance {
 
-	public enum Option {
-		TOTAL_ALIVE, TOTAL_AREA, BOTH
-	}
-
 	/**
-	 * This program allows the user to fetch the specified column of a file.
+	 * This method allows the user to fetch the specified column of a file.
 	 * 
 	 * @param file                Data, user wants to fetch the specified column
 	 *                            from.
@@ -28,21 +21,21 @@ public class QualityAssurance {
 	 * @return Double values from the specified column in a list.
 	 * @throws IOException
 	 */
-	private static List<Double> loadValuesFromColumn(File file, int columnIndex, String delimiter,
-			int numbersOfLinesToSkip) throws IOException, NumberFormatException {
+	public static List<Double> loadValuesFromColumn(File file, int columnIndex, String delimiter,
+			int numberOfLinesToSkip) throws IOException, NumberFormatException {
 
 		List<Double> valuesList = new ArrayList<>();
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		String line = null;
 		int lineCounter = 0;
 
-		// Skip first two lines.
+		// Skip the first two lines.
 		for (int i = 0; i < 2; i++) {
 			reader.readLine();
 		}
 
 		// This step is used for the computation of the total area.
-		for (int i = 0; i < numbersOfLinesToSkip; i++) {
+		for (int i = 0; i < numberOfLinesToSkip; i++) {
 			reader.readLine();
 		}
 
@@ -70,8 +63,20 @@ public class QualityAssurance {
 		return valuesList;
 	}
 
-	private static List<Double> loadTimeLimitsFromColumn(File file, int columnIndex, String delimiter, int sumOfCentres)
-			throws IOException, NumberFormatException {
+	/**
+	 * This method allows the user to fetch the first and the second last
+	 * elimination time for "building" the intervals.
+	 * 
+	 * @param file
+	 * @param columnIndex
+	 * @param delimiter
+	 * @param sumOfCentres
+	 * @return
+	 * @throws IOException
+	 * @throws NumberFormatException
+	 */
+	public static List<Double> loadFirstAndSecondLastEliminationTimesFromColumn(File file, int columnIndex,
+			String delimiter, int sumOfCentres) throws IOException, NumberFormatException {
 
 		List<Double> valuesList = new ArrayList<>();
 		BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -109,6 +114,19 @@ public class QualityAssurance {
 		return valuesList;
 	}
 
+	/**
+	 * This method allows the user to compute the number of total alive centers for
+	 * a given time.
+	 * 
+	 * @param file
+	 * @param columnIndexForEliminationTime
+	 * @param delimiter
+	 * @param timeLimit
+	 * @param eliminationTimeList
+	 * @return
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
 	public static int getTotalAlive(File file, int columnIndexForEliminationTime, String delimiter, double timeLimit,
 			List<Double> eliminationTimeList) throws NumberFormatException, IOException {
 
@@ -123,6 +141,22 @@ public class QualityAssurance {
 		return totalAlive;
 	}
 
+	/**
+	 * This method allows the user to compute the total area of the total alive
+	 * centers for a given time.
+	 * 
+	 * @param file
+	 * @param columnIndexForEliminationTime
+	 * @param columnIndexForRadius
+	 * @param delimiter
+	 * @param timeLimit
+	 * @param eliminationTimeList
+	 * @param radiiList
+	 * @param sumOfCenters
+	 * @return
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
 	public static double getTotalArea(File file, int columnIndexForEliminationTime, int columnIndexForRadius,
 			String delimiter, double timeLimit, List<Double> eliminationTimeList, List<Double> radiiList,
 			int sumOfCenters) throws NumberFormatException, IOException {
@@ -140,170 +174,5 @@ public class QualityAssurance {
 			area = area + 2 * PI * Math.pow(radius, 2);
 		}
 		return area;
-	}
-
-	public static void main(String[] args) {
-
-		String[] fileNames = { "result_default.ce", "result_radius.ce", "result_in_range.ce", "result_osm_id.ce",
-				"result_random.ce" };
-		File resultFile = new File("result_for_total_alive.csv");
-
-		// TODO:
-		final int sumOfCenters = 184;
-
-		// Choose the property, you want to calculate.
-		Option option = Option.TOTAL_ALIVE;
-
-		// Construct understandable names for the used heuristics.
-		ArrayList<String> heuristics = new ArrayList<String>();
-
-		for (String fileName : fileNames) {
-			fileName = fileName.replace("result_", "").replace(".ce", "").toUpperCase();
-			heuristics.add(fileName);
-		}
-
-		// Construct the header of the used heuristics for the .csv file.
-		String stringOfHeuristics = "";
-
-		for (int i = 0; i < heuristics.size(); i++) {
-			if (i < heuristics.size() - 1) {
-				stringOfHeuristics += heuristics.get(i) + ";";
-			} else {
-				stringOfHeuristics += heuristics.get(i);
-			}
-		}
-
-		List<Double> valuesList = null;
-
-		final int columnIndexForEliminationTime = 4;
-		final int columnIndexForRadius = 5;
-		int totalAlive = 0;
-		double totalArea = 0.0;
-
-		// Set the total number of time limits.
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter the maximum number of time limits.");
-		int numberOfTimeLimits = input.nextInt();
-		double[] arrayOfTimeLimits = new double[numberOfTimeLimits + 1];
-
-		File inputFileForTimeLimits = new File("result_default.ce");
-
-		// Get time limits, dynamically.
-		try {
-			List<Double> timeLimitsList = loadTimeLimitsFromColumn(inputFileForTimeLimits,
-					columnIndexForEliminationTime, " ", sumOfCenters);
-			double intervall = (timeLimitsList.get(1) - timeLimitsList.get(0)) / numberOfTimeLimits;
-			for (int i = 1; i <= numberOfTimeLimits; i++) {
-				double timeLimit = intervall * i;
-				arrayOfTimeLimits[i] = timeLimit;
-			}
-		} catch (NumberFormatException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		input.close();
-
-		// From here on, the options are specified.
-		if (option == Option.TOTAL_ALIVE) {
-
-			try {
-				String saveResult = "";
-				BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
-				saveResult = "TOTAL_ALIVE" + "\n" + "TIME_LIMIT;" + stringOfHeuristics;
-
-				for (double timeLimit : arrayOfTimeLimits) {
-					saveResult = saveResult + "\n" + timeLimit;
-					for (String fileName : fileNames) {
-						File inputFile = new File(fileName);
-
-						try {
-							totalAlive = getTotalAlive(inputFile, columnIndexForEliminationTime, " ", timeLimit,
-									valuesList);
-
-							saveResult = saveResult + ";" + totalAlive;
-
-						} catch (Exception e) {
-							System.err.println("Da ist was schiefgegangen: " + e.getMessage());
-						}
-					}
-				}
-				writer.write(saveResult);
-				writer.close();
-				System.out.println("Calculation of total alive has been finished.");
-			} catch (IOException e) {
-				System.out.println("File I/O error!");
-			}
-		}
-
-		else if (option == Option.TOTAL_AREA) {
-
-			try {
-				String saveResult = "";
-				BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
-				saveResult = "TOTAL_AREA" + "\n" + "TIME_LIMIT;" + stringOfHeuristics;
-
-				for (double timeLimit : arrayOfTimeLimits) {
-					saveResult = saveResult + "\n" + timeLimit;
-					for (String fileName : fileNames) {
-						File inputFile = new File(fileName);
-
-						try {
-							totalArea = getTotalArea(inputFile, columnIndexForEliminationTime, columnIndexForRadius,
-									" ", timeLimit, valuesList, valuesList, sumOfCenters);
-
-							saveResult = saveResult + ";" + totalArea;
-
-						} catch (Exception e) {
-							System.err.println("Da ist was schiefgegangen: " + e.getMessage());
-						}
-					}
-				}
-				writer.write(saveResult);
-				writer.close();
-				System.out.println("Calculation of total area has been finished.");
-			} catch (IOException e) {
-				System.out.println("File I/O error!");
-			}
-		}
-
-		else {
-			if (fileNames.length == 1) {
-				for (String fileName : fileNames) {
-					File inputFile = new File(fileName);
-
-					try {
-						String saveResult = "";
-						BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
-						saveResult = "TOTAL_ALIVE AND TOTAL_AREA" + "\n" + "TIME_LIMIT;TOTAL_ALIVE;TOTAL_AREA";
-
-						for (double timeLimit : arrayOfTimeLimits) {
-
-							try {
-								totalAlive = getTotalAlive(inputFile, columnIndexForEliminationTime, " ", timeLimit,
-										valuesList);
-							} catch (Exception e) {
-								System.err.println("Da ist was schiefgegangen: " + e.getMessage());
-							}
-
-							try {
-								totalArea = getTotalArea(inputFile, columnIndexForEliminationTime, columnIndexForRadius,
-										" ", timeLimit, valuesList, valuesList, sumOfCenters);
-							} catch (Exception e) {
-								System.err.println("Da ist was schiefgegangen: " + e.getMessage());
-							}
-							saveResult = saveResult + "\n" + timeLimit + ";" + totalAlive + ";" + totalArea;
-						}
-						writer.write(saveResult);
-						writer.close();
-						System.out.println("Calculation of total alive and total area has been finished.");
-					} catch (IOException e) {
-						System.out.println("File I/O error!");
-					}
-				}
-			} else
-				System.out.println("Error: More than one file !");
-		}
 	}
 }
